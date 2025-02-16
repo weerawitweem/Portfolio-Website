cf95620252693c1b644d957b65109bc05592fe43
Weerawit Pungpiboon <weerawitweem@gmail.com>
2025-02-16 11:55:19 +0700

    Add model to website

--- main.js
+++ main.js
@@ -26,6 +26,19 @@ const mouse = new THREE.Vector2();
 
 //-----------------------------------------------------------------------------------------------------------------------//
 // Object
+const loader = new GLTFLoader();
+
+loader.load( 'Assets/model/room.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+    gltf.scene.position.set(0, -6, 3);
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
+
 const cube1 = new THREE.Mesh(new THREE.BoxGeometry(0.5,0.5,0.5), new THREE.MeshLambertMaterial( {color : 0x32a852} ));
 cube1.name = "introbook";
 cube1.position.x = 0;
@@ -61,17 +74,47 @@ scene.add( light );
 // Middle Content
 // State  <none> - <introduction> - <python project> - <c/c++ experience> - <java project>
 let isViewing = false;
+let canClick = true;
+const middleContent = document.getElementById("middle");
 
 function closePopup() {
+    canClick=false;
     console.log("Close");
+    gsap.to("#middle", { duration: 0.5, opacity: 0, onComplete: function() {
+        middleContent.innerHTML = '';
+        canClick=true;
+    }});
+    
 }
 
 function openIntro() {
     console.log("open intro");
+    middleContent.innerHTML = `
+        <img src="Assets/img/portrait.jpg" alt="lame ahh portrait" id="Intro-Img">
+            <p id="Intro-Text">
+                Hello
+                My name is Weerawit Pungpiboon
+                or Weem.<br>Well, I like creating cool thing!
+                <br>I used to make small game, write weird website. 
+                <br>Right now I'm studying computer engineer 
+                <br>So I hope I'll learn more cool stuff!!
+            </p>
+            <p>please ignore this image I'll change I promise</p>
+    `;
+    gsap.to("#middle", { duration: 2, opacity: 1 });
 }
 
 function openPython() {
     console.log("open python");
+    middleContent.innerHTML = `
+        <p id="Python-Text"> 
+            I have experience coding discord bot using python.<br>
+            This bot simply get homework from discord server user <br>
+            and will notify when it near due date.<br>
+            This is my project for class when I was in 2nd year highschool.
+        </p>
+    `;
+    gsap.to("#middle", { duration: 2, opacity: 1 });
 }
 
 function openCCPP() {
@@ -80,6 +123,12 @@ function openCCPP() {
 
 function openJava() {
     console.log("open java");
+    middleContent.innerHTML = `
+        <p id="Java-Text">
+            I'm still learning java so I do not have any finished project right now :/ 
+        </p>
+    `;
+    gsap.to("#middle", { duration: 2, opacity: 1 });
 }
 //-----------------------------------------------------------------------------------------------------------------------//
 
@@ -122,10 +171,13 @@ document.addEventListener('mousemove', (event) => {
 
     //Raycast
     rayCaster.setFromCamera(mouse, camera);
-
+    const computedStyle = window.getComputedStyle(middleContent);
+    const opacity = computedStyle.getPropertyValue('opacity');
+    console.log(opacity);
 });
 
 document.addEventListener('click', (event) => {
+    if (!canClick) return;
     const intersects = rayCaster.intersectObjects( scene.children );
     for (let i=0;i<intersects.length;i++) {
         console.log( intersects[i].name );
