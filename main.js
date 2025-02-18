0dbeeb20f7b00a5765bb524e434ae52daa585033
Weerawit Pungpiboon <weerawitweem@gmail.com>
2025-02-18 17:26:17 +0700

    Finish Intro book

--- main.js
+++ main.js
@@ -19,55 +19,51 @@ const rayCaster = new THREE.Raycaster();
 // Cursor
 const mouse = new THREE.Vector2();
 
-// Loader
-//const loader = THREE.GLTFLoader();
 //-----------------------------------------------------------------------------------------------------------------------//
-
 
 //-----------------------------------------------------------------------------------------------------------------------//
 // Object
 const loader = new GLTFLoader();
+let book, python, ccpp, java;
 
 loader.load( 'Assets/model/room.glb', function ( gltf ) {
 
-	scene.add( gltf.scene );
+    scene.add( gltf.scene );
     gltf.scene.position.set(0, 0, 0);
-
+    
 }, undefined, function ( error ) {
-
-	console.error( error );
-
+    
+    console.error( error );
+    
 } );
 
-const cube1 = new THREE.Mesh(new THREE.BoxGeometry(0.5,0.5,0.5), new THREE.MeshLambertMaterial( {color : 0x32a852} ));
-cube1.name = "introbook";
-cube1.position.x = 0;
-scene.add( cube1 );
+loader.load( 'Assets/model/book.glb', function ( gltf ) {
 
-const cube2 = new THREE.Mesh(new THREE.BoxGeometry(0.5,0.5,0.5), new THREE.MeshLambertMaterial( {color : 0x32a852} ));
-cube2.name = "pythonProject";
-cube2.position.x = 1;
-scene.add( cube2 );
+    book = gltf.scene;
+    book.name = "introbook";
+    scene.add(book);
+    book.position.set(0.1, 4.1, -3.5);
+    book.rotation.set(0.1,0,0);
 
-const cube3 = new THREE.Mesh(new THREE.BoxGeometry(0.5,0.5,0.5), new THREE.MeshLambertMaterial( {color : 0x32a852} ));
-cube3.name = "ccppProject";
-cube3.position.x = 2;
-scene.add( cube3 );
+    // book.position.set(0.1, 5, 3.25);
+    // book.rotation.set(0.8,0,0);
+    
+}, undefined, function ( error ) {
+    
+    console.error( error );
+    
+} );
 
-const cube4 = new THREE.Mesh(new THREE.BoxGeometry(0.5,0.5,0.5), new THREE.MeshLambertMaterial( {color : 0x32a852} ));
-cube4.name = "javaProject";
-cube4.position.x = 3;
-scene.add( cube4 );
-
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5), new THREE.MeshPhongMaterial( {color : 0x3246a8} ));
-sphere.position.x = -2;
-sphere.name = "sphere";
-scene.add( sphere );
+const bookHitbox = new THREE.Mesh(new THREE.BoxGeometry(1.5, 1, 1), new THREE.MeshBasicMaterial({color: 0x32a852,transparent: true, opacity:0}));
+bookHitbox.name = "introbook";
+bookHitbox.position.set(0, 4.5, 0);
+scene.add( bookHitbox );
 
 const light = new THREE.DirectionalLight(0xffffff, 5);
-light.position.set(1,8,2);
+light.position.set(4,3,2);
 
 scene.add( light );
+
 const light2 = new THREE.DirectionalLight(0xffffff, 5);
 light2.position.set(1,8,2);
 
@@ -85,88 +81,112 @@ function closePopup() {
     canClick=false;
     console.log("Close");
     gsap.to("#middle", { duration: 0.5, opacity: 0, onComplete: function() {
-        middleContent.innerHTML = '';
+        // middleContent.innerHTML = '';
         canClick=true;
+        
+        gsap.to(book.position, {
+            duration:1,
+            x:0.1,
+            y:4.1,
+            z:-3.5
+        });
+        gsap.to(book.rotation, {
+            duration:1,
+            x:0.1,
+            y:0,
+            z:0,
+        });
+
     }});
-    
 }
 
 function openIntro() {
-    console.log("open intro");
-    middleContent.innerHTML = `
-        <img src="Assets/img/portrait.jpg" alt="lame ahh portrait" id="Intro-Img">
-            <p id="Intro-Text">
-                Hello
-                My name is Weerawit Pungpiboon
-                or Weem.<br>Well, I like creating cool thing!
-                <br>I used to make small game, write weird website. 
-                <br>Right now I'm studying computer engineer 
-                <br>So I hope I'll learn more cool stuff!!
-            </p>
-            <p>please ignore this image I'll change I promise</p>
-    `;
-    gsap.to("#middle", { duration: 2, opacity: 1 });
+    gsap.to(book.position, {
+        duration:1,
+        x:0.1,
+        y:5,
+        z:3.25
+    });
+    gsap.to(book.rotation, {
+        duration:1,
+        x:0.8,
+        y:0,
+        z:0,
+        onComplete: () => {
+    
+        console.log("open intro");
+        middleContent.innerHTML = `
+        <div id="Intro-Container">
+                <div id="Intro-Img-Container">
+                    <img src="Assets/img/portrait.jpg" alt="lame ahh portrait" id="Intro-Img">
+                </div>
+                <div id="Intro-Text-Container">
+                    <p id="Intro-Text">
+                            Hello
+                            My name is Weerawit Pungpiboon
+                            or Weem.<br>Well, I like creating cool thing!
+                            <br>I used to make small game, write weird website. 
+                            <br>Right now I'm studying computer engineer 
+                            <br>So I hope I'll learn more cool stuff!!
+                    </p>
+                    <p>please ignore this image I'll change I promise</p>
+                </div>
+            </div>
+        `;
+        gsap.to("#middle", { duration: 2, opacity: 1 });
+        
+        }
+    });
 }
 
 function openPython() {
     console.log("open python");
     middleContent.innerHTML = `
         <p id="Python-Text"> 
-            I have experience coding discord bot using python.<br>
-            This bot simply get homework from discord server user <br>
-            and will notify when it near due date.<br>
-            This is my project for class when I was in 2nd year highschool.
+        I have experience coding discord bot using python.<br>
+        This bot simply get homework from discord server user <br>
+        and will notify when it near due date.<br>
+        This is my project for class when I was in 2nd year highschool.
         </p>
-    `;
-    gsap.to("#middle", { duration: 2, opacity: 1 });
-}
-
-function openCCPP() {
-    console.log("open c/cpp");
-}
-
-function openJava() {
-    console.log("open java");
-    middleContent.innerHTML = `
+        `;
+        gsap.to("#middle", { duration: 2, opacity: 1 });
+    }
+    
+    function openCCPP() {
+        console.log("open c/cpp");
+    }
+    
+    function openJava() {
+        console.log("open java");
+        middleContent.innerHTML = `
         <p id="Java-Text">
-            I'm still learning java so I do not have any finished project right now :/ 
+        I'm still learning java so I do not have any finished project right now :/ 
         </p>
-    `;
-    gsap.to("#middle", { duration: 2, opacity: 1 });
-}
+        `;
+        gsap.to("#middle", { duration: 2, opacity: 1 });
+    }
 //-----------------------------------------------------------------------------------------------------------------------//
-
+    
 //-----------------------------------------------------------------------------------------------------------------------//
-// Camera
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0,5,5);
-let cameraView = 1;
-
-function gotoExpAndProject() {
+    // Camera
+    const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0,5,5);
+    let cameraView = 1;
+    
+    function gotoExpAndProject() {
     gsap.to(camera.position, {
         duration:1.5,
-        x:-2,
+        x:-3,
         y:5,
-        z:2
+        z:0.5
     });
-    const leftRotation = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), 2);
-    const downRotation = new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(1, 0, 0), 1);
-
-    // Combine the rotations
-    const targetRotation = new THREE.Quaternion();
-    targetRotation.multiplyQuaternions(leftRotation, downRotation);
-    targetRotation.multiplyQuaternions(targetRotation, camera.quaternion);
-
-    // Animate the camera's quaternion (rotation)
-    gsap.to(camera.quaternion, {
-        duration: 1.5,
-        x: targetRotation.x,
-        y: targetRotation.y,
-        z: targetRotation.z,
-        w: targetRotation.w
+    gsap.to(camera.rotation, {
+        duration:1.5,
+        x:-0.5,
+        y:0.8,
+        z:0.375
     });
-
-
+    
     cameraView = 0;
     console.log("gotopro");
 }
@@ -191,9 +211,15 @@ function gotoIntro() {
 function gotoMore() {
     gsap.to(camera.position, {
         duration:1.5,
-        x:2,
+        x:5,
         y:5,
-        z:5
+        z:0
+    });
+    gsap.to(camera.rotation, {
+        duration:1.5,
+        x:0,
+        y:-0.75,
+        z:0
     });
     cameraView = 2;
     console.log("gomore");
@@ -203,8 +229,12 @@ function turnLeft() {
     console.log("left");
     if (cameraView == 1) {
         gotoExpAndProject();
+        document.getElementById("left-button").querySelector(".Button-Text").textContent = "No more!";
+        document.getElementById("right-button").querySelector(".Button-Text").textContent = "Introduction";
     } else if (cameraView == 2) {
         gotoIntro();
+        document.getElementById("left-button").querySelector(".Button-Text").textContent = "Project";
+        document.getElementById("right-button").querySelector(".Button-Text").textContent = "More about me";
     }
 }
 
@@ -212,36 +242,32 @@ function turnRight() {
     console.log("right");
     if (cameraView == 0) {
         gotoIntro();
+        document.getElementById("left-button").querySelector(".Button-Text").textContent = "Project";
+        document.getElementById("right-button").querySelector(".Button-Text").textContent = "More about me";
     } else if (cameraView == 1) {
         gotoMore();
+        document.getElementById("left-button").querySelector(".Button-Text").textContent = "Introduction";
+        document.getElementById("right-button").querySelector(".Button-Text").textContent = "No more!";
     }
 }
 
 function cameraAnimate() {
-    // gsap.to(camera.rotation, {
-    //     x: mouse.y * 0.1, // Adjust the multiplier for rotation intensity
-    //     y: -mouse.x * 0.1, // Adjust the multiplier for rotation intensity
-    //     duration: 0.5,
-    //     onUpdate: function() {
-    //       // Ensure the camera matrix is updated
-    //       camera.updateProjectionMatrix();
-    //     }
-    //   });
+    let lookSensitive = 0.1;
+    //idk how to implement
 }
 //-----------------------------------------------------------------------------------------------------------------------//
-
-
+    
+    
 //-----------------------------------------------------------------------------------------------------------------------//
 //Event
 document.addEventListener('mousemove', (event) => {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
+    
     //Raycast
     rayCaster.setFromCamera(mouse, camera);
     const computedStyle = window.getComputedStyle(middleContent);
     const opacity = computedStyle.getPropertyValue('opacity');
-    console.log(opacity);
 });
 
 document.addEventListener('click', (event) => {
@@ -287,7 +313,6 @@ document.addEventListener("DOMContentLoaded", function() {
     rightbutton.addEventListener("click", turnRight);
 });
 //-----------------------------------------------------------------------------------------------------------------------//
-
 
 //-----------------------------------------------------------------------------------------------------------------------//
 // Render
